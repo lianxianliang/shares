bd5762469bc501d020474d863fac007574a88bbf
lianxianliang <lianxl@sunmnet.com>
2019-03-14 19:49:00 +0800

    新增表格，修复实际价小于预测价时，运算不正确的bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -43,13 +43,13 @@
               class="rateBox"
             >
               <el-input-number
-                v-model="form.end_price_forecast"
+                v-model="form.end_price_pre"
                 :precision="2"
                 :step="0.01"
                 :min="0"
                 @change="handleChange"
               ></el-input-number>
-              <div class="rate price">( {{(((form.end_price_forecast / form.support_price - 1) * 100)).toFixed(2) + "%"}} )</div>
+              <div class="rate price">( {{(((form.end_price_pre / form.support_price - 1) * 100)).toFixed(2) + "%"}} )</div>
             </el-form-item>
 
             <el-form-item
@@ -57,7 +57,7 @@
               class="rateBox"
             >
               <el-input-number
-                v-model="form.sell_price"
+                v-model="form.sell_price_pre"
                 :precision="2"
                 :step="0.01"
                 :min="0"
@@ -155,27 +155,27 @@
               class="info forcast"
             >
               <el-form-item label="预计交易次数">
-                <div>{{this.count}}</div>
+                <div>{{this.count_pre}}</div>
               </el-form-item>
 
               <el-form-item label="预计投入">
-                <div>{{this.Principal}}</div>
+                <div>{{this.Principal_pre}}</div>
               </el-form-item>
 
               <el-form-item label="预计收益">
-                <div>{{this.profit.toFixed(0)}}</div>
+                <div>{{this.profit_pre.toFixed(0)}}</div>
               </el-form-item>
 
               <el-form-item label="预计收益率">
-                <div>{{this.Principal===0?'0':((this.profit / this.Principal) * 100).toFixed(2)}}%</div>
+                <div>{{this.Principal_pre===0?'0':((this.profit_pre / this.Principal_pre) * 100).toFixed(2)}}%</div>
               </el-form-item>
 
               <el-form-item label="预计投资周期">
-                <div>{{this.cycle}}天</div>
+                <div>{{this.cycle_pre}}天</div>
               </el-form-item>
 
               <el-form-item label="预计日均">
-                <div>{{this.cycle===0?'0':(this.profit / this.cycle).toFixed(2)}}</div>
+                <div>{{this.cycle_pre===0?'0':(this.profit_pre / this.cycle_pre).toFixed(2)}}</div>
               </el-form-item>
             </el-form>
           </el-col>
@@ -259,24 +259,48 @@
             align="center"
           >
           </el-table-column>
+          <!-- <el-table-column
+            prop="rate_3"
+            label="相差"
+            align="center"
+          >
+          </el-table-column> -->
         </el-table>
       </el-col>
     </el-row>
+
+    <div
+      id="myChart"
+      :style="{width: '100%', height: '420px'}"
+    ></div>
   </div>
 
 </template>
 
 <script>
+// 引入基本模板
+let echarts = require('echarts/lib/echarts')
+// 引入柱状图组件
+require('echarts/lib/chart/line')
+// 引入提示框和title组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
+
 export default {
   data() {
     return {
+      xAxis: [],
+      dataReal: [],
+      dataPre: [],
       // 可动用资产
       money: 500000,
       // 定投次数
       frequency: 20,
 
+      // 是否已到达预计终止买入价格
+      isEndPre: false,
       // 是否已到达实际终止买入价格
-      isEnd: false,
+      isEndReal: false,
 
       // 交易次数
       count: 0,
@@ -290,6 +314,19 @@ export default {
       cur_shares: 0,
       // 投资周期
       cycle: 0,
+
+      // 预计交易次数
+      count_pre: 0,
+      // 预计成本价
+      cost_price_pre: 0,
+      // 预计投入
+      Principal_pre: 0,
+      // 预计利润
+      profit_pre: 0,
+      // 预计持股
+      cur_shares_pre: 0,
+      // 预计投资周期
+      cycle_pre: 0,
 
       // 实际交易次数
       count_real: 0,
@@ -308,26 +345,23 @@ export default {
         // 开始买入价格(支撑位)
         support_price: 10,
         // 预计终止价格
-        end_price_forecast: 7,
+        end_price_pre: 7,
         // 实际终止价格
         end_price_real: 8,
         // 定投间隔
         diff: 0,
         // 预计卖出价格
-        sell_price: 0,
+        sell_price_pre: 0,
         // 实际卖出价格
         sell_price_real: 0,
         // 持有日期
-        date: [new Date(), new Date(+new Date() + 7 * 24 * 60 * 60 * 1000)],
+        date: [new Date(), new Date(+new Date() + 6 * 24 * 60 * 60 * 1000)],
       },
       tableData: []
     }
   },
   mounted() {
-    this.form.sell_price = (this.form.end_price_forecast + (this.form.support_price - this.form.end_price_forecast) * 2 / 3).toFixed(2);
-    this.form.sell_price_real = (this.form.end_price_real + (this.form.support_price - this.form.end_price_real) * 2 / 3).toFixed(2);
-
-    this.form.diff = ((this.form.support_price - this.form.end_price_forecast) / (this.frequency - 1)).toFixed(2) - 0;
+    this.handleChange();
   },
   methods: {
     handleChange() {
@@ -337,14 +371,14 @@ export default {
     },
     // 重置为系统默认预计卖出价格
     handleClickSellPrice() {
-      this.form.sell_price = (this.form.end_price_forecast + (this.form.support_price - this.form.end_price_forecast) * 2 / 3).toFixed(2);
+      this.form.sell_price_pre = (this.form.end_price_pre + (this.form.support_price - this.form.end_price_pre) * 2 / 3).toFixed(2);
     },
     handleClickSellPriceReal() {
       this.form.sell_price_real = (this.form.end_price_real + (this.form.support_price - this.form.end_price_real) * 2 / 3).toFixed(2);
     },
     // 重置为系统默认定投间隔
     handleClickDiff() {
-      this.form.diff = ((this.form.support_price - this.form.end_price_forecast) / (this.frequency - 1)).toFixed(2) - 0;
+      this.form.diff = ((this.form.support_price - this.form.end_price_pre) / (this.frequency - 1)).toFixed(2) - 0;
     },
     onSubmit() {
 
@@ -368,8 +402,13 @@ export default {
       // 持股
       this.cur_shares = 0;
 
-      this.isEnd = false;
+      this.isEndPre = false;
+      this.isEndReal = false;
       this.tableData = [];
+      this.handleClickDiff();
+      this.xAxis = [];
+      this.dataReal = [];
+      this.dataPre = [];
       this.cal(support_price + this.form.diff);
 
     },
@@ -380,17 +419,26 @@ export default {
       // 买入价
       let buy_price = (cur_price - this.form.diff).toFixed(2);
 
+      let min = Math.min(this.form.end_price_pre, this.form.end_price_real);
+
       // 预计
-      if (buy_price < this.form.end_price_forecast) {
+      if (!this.isEndPre && buy_price < this.form.end_price_pre) {
+        this.count_pre = this.count;
+        this.cost_price_pre = this.cost_price;
+        this.Principal_pre = this.Principal;
         // 市值
-        let value = this.cur_shares * (this.form.sell_price - this.cost_price);
+        let value = this.cur_shares * (this.form.sell_price_pre - this.cost_price);
         // 收益
-        this.profit = value - Math.ceil(value / 10000) * 5;
-        return;
+        this.profit_pre = value - Math.ceil(value / 10000) * 5;
+        this.cur_shares_pre = this.cur_shares;
+        this.cycle_pre = this.cycle;
+
+        this.isEndPre = true;
+
       }
 
       // 实际
-      if (!this.isEnd && buy_price < this.form.end_price_real) {
+      if (!this.isEndReal && buy_price < this.form.end_price_real) {
         this.count_real = this.count;
         this.cost_price_real = this.cost_price;
         this.Principal_real = this.Principal;
@@ -401,7 +449,12 @@ export default {
         this.cur_shares_real = this.cur_shares;
         this.cycle_real = this.cycle;
 
-        this.isEnd = true;
+        this.isEndReal = true;
+      }
+
+      if (buy_price < min) {
+        this.drawLine();
+        return;
       }
 
       // 买入股票
@@ -415,6 +468,7 @@ export default {
       // 投入
       this.Principal = this.Principal + curPrincipal + Math.ceil(curPrincipal / 10000) * 5;
       if (buy_price <= 0 || this.Principal >= this.money) {
+        this.drawLine();
         return;
       }
 
@@ -427,6 +481,8 @@ export default {
       // 自支撑位下跌
       let rate_2 = ((buy_price / this.form.support_price - 1) * 100).toFixed(2) + "%";
 
+      let rate_3 = (((buy_price / this.cost_price - 1) * 100) - ((buy_price / this.form.support_price - 1) * 100)).toFixed(2) + "%";
+
       let obj = {
         buy_shares,
         cur_shares: this.cur_shares,
@@ -435,17 +491,73 @@ export default {
         rate: (buy_price - this.cost_price).toFixed(4),
         buy_price,
         rate_1,
-        rate_2
+        rate_2,
+        rate_3,
       }
 
-      if (!this.isEnd && buy_price - this.diff < 0) {
-        obj.isEnd = true;
+      this.xAxis.push(this.count);
+      this.dataReal.push(obj.cost_price);
+      this.dataPre.push(obj.buy_price);
+
+      if (!this.isEndReal && buy_price - this.diff < 0) {
+        obj.isEndReal = true;
       }
 
       this.tableData.push(obj);
 
+      // this.form.diff = this.form.diff * 1.05;
       this.cal(buy_price);
+    },
+
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = echarts.init(document.getElementById('myChart'))
+      // 绘制图表
+      let option = {
+        title: {
+          text: ''
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['成本价', '买入价']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: this.xAxis
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '成本价',
+            type: 'line',
+            data: this.dataReal
+          },
+          {
+            name: '买入价',
+            type: 'line',
+            data: this.dataPre
+          }
+        ]
+      };
+      myChart.setOption(option);
     }
+
   }
 }
 </script>
